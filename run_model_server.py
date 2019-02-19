0c62cf16eb1d6c888f91bd89a466ec7e433ab920
LeeKLTW <LeeKLTW@gmail.com>
2019-02-19 15:23:14 +0800

    server classify_process, predict, main

--- run_model_server.py
+++ run_model_server.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+from PIL import Image
 from threading import Thread
 import numpy as np
 import base64
@@ -21,7 +22,7 @@ IMAGE_CHANS = 3
 IMAGE_DTYPE = "float32"
 
 # initialize constants used for server queueing
-IMAGE_QUESE = "image_queue"  # key name store in redis
+IMAGE_QUEUE = "image_queue"  # key name store in redis
 BATCH_SIZE = 32
 SEVER_SLEEP = 0.25
 CLIENT_SLEEP = 0.25
@@ -60,7 +61,7 @@ def classify_process():
     model = ResNet50(weights='imagenet')
 
     while True:
-        queue = db.lrange(IMAGE_QUESE, 0, BATCH_SIZE - 1)
+        queue = db.lrange(IMAGE_QUEUE, 0, BATCH_SIZE - 1)
         imageIDs = []
         batch = None
 
@@ -72,5 +73,53 @@ def classify_process():
                 batch = image
             else:
                 batch = np.vstack([batch, image])
-
             imageIDs.append(q["id"])
+
+            if len(imageIDs) > 0:
+                print("* Batch size: {}".format(batch.shape))
+                preds = model.predict(batch)
+                results = decode_predictions(preds)
+
+                for (imageID, resultSet) in zip(imageIDs, results):
+                    output = []
+                    for (imagenetID, label, prob) in resultSet:
+                        r = {"label":label,"probability":float(prob)}
+                        output.append(r)
+                    db.set(imageID, json.dumps(output))
+
+                db.ltrim(IMAGE_QUEUE,len(imageIDs),-1)
+            time.sleep(SEVER_SLEEP)
+
+@app.route("/predict",method=["POST"])
+def predict():
+    data = {"success": False}
+    if flask.request.method == "POST":
+        if flask.request.files.get('image'):
+            image = flask.request.files['image'].read()
+            image = Image.open(io.BytesIO(image))
+            image = prepare_image(image,(IMAGE_WIDTH,IMAGE_HEIGHT))
+            image = image.copy(order="C")
+            
+            k = str(uuid.uuid4())
+            d = {"id":k, "image": base64_encode_image(image)}
+            db.rpush(IMAGE_QUEUE, json.dump(d))
+
+            while True:
+                output = db.get(k)
+                if output is not None:
+                    output = output.decode("utf-8")
+                    data["prediction"] = json.load(output)
+                    db.delete(k)
+                    break
+                time.sleep(CLIENT_SLEEP)
+            data["success"] = True
+    return flask.jsonify(data)
+
+
+if __name__ == '__main__':
+    print("* Start ResNet.")
+    t = Thread(target=classify_process, args=())
+    t.daemon = True
+    t.start()
+    print("* Start Restful API")
+    app.run()
