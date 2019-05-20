4dd8889f0bb7c6ad1cb20556f811e788340eb240
LeeKLTW <LeeKLTW@gmail.com>
2019-05-20 18:53:53 +0800

    need to reshape for tensorflow input type

--- transformer.py
+++ transformer.py
@@ -24,6 +24,7 @@ np.random.seed(42)
 NUM_WORDS = max([len(sent) for sent in x_train])  # 2376
 MAX_LEN = 32
 
+
 def idx2word(idx):
     return [w for w in word_index if word_index[w] == idx][0]
 
@@ -43,17 +44,16 @@ def _pad_x(x):
 
 
 class MultiHeadAttention:
-    def __init__(self, n_head=8, d_model=512, dropout=0.1,maxlen=MAX_LEN, **kwargs):
+    def __init__(self, n_head=8, d_model=512, dropout=0.1, maxlen=MAX_LEN, **kwargs):
         ""
         self.n_head = n_head
         self.d_model = d_model
         self.d_k = self.d_v = d_model // n_head  # 3.2.2 #dimentin of embedding
         self.dropout = dropout
         self.maxlen = maxlen
-        self.wqi = keras.layers.Dense(self.d_model,use_bias=False)
-        self.wki = keras.layers.Dense(self.d_model,use_bias=False)
-        self.wvi = keras.layers.Dense(self.d_model,use_bias=False)
-
+        self.wqi = keras.layers.Dense(self.d_model, use_bias=False)
+        self.wki = keras.layers.Dense(self.d_model, use_bias=False)
+        self.wvi = keras.layers.Dense(self.d_model, use_bias=False)
 
     def __call__(self, q, k, v, mask=None):
         """
@@ -61,28 +61,34 @@ class MultiHeadAttention:
         v.shape = (batch_size,maxlen, d_k)
         k.shape = (batch_size,maxlen, d_k)
         """
+
         def _reshape1(x):
-            x = tf.reshape(x)
+            shape = tf.reshape(x)  # [batch_size, maxlen, n_head * d_k]
+            x = tf.reshape(x, shape=(-1, shape[1], self.n_head, d_k))  # [batch_size, maxlen, n_head, d_k]
+            x = tf.transpose(x, (2, 0, 1, 3))  # [n_head,batch_size,maxlen,d_k]
+            x = tf.reshape(x, (-1, shape[1], d_k)) # [n_head*batch_size, max_len, d_k]
+            return x
 
         # multihead attention
+        d_k, d_v = self.d_k, self.d_k
         dvd = np.sqrt(self.d_k)
 
+        #todo continue here
         q = self.wqi(q)
         k = self.wki(k)
         v = self.wvi(v)
 
-        attn = keras.layers.Lambda(lambda x:K.batch_dot(x[0],x[1],axes=[2,2])/dvd)([q, k])
+        attn = keras.layers.Lambda(lambda x: K.batch_dot(x[0], x[1], axes=[2, 2]) / dvd)([q, k])
 
         if mask is not None:
-            mmask = keras.layers.Lambda(lambda x:(-1e+10)*(1-x))(mask)
+            mmask = keras.layers.Lambda(lambda x: (-1e+10) * (1 - x))(mask)
             attn = keras.layers.Add()([attn, mmask])
         attn = keras.layers.Activation('softmax')(attn)
-        attn = keras.layers.Lambda(lambda x:K.batch_dot(x[0], x[1]))([attn, v])
+        attn = keras.layers.Lambda(lambda x: K.batch_dot(x[0], x[1]))([attn, v])
         return attn
 
     def compute_output_shape(self, input_shape):
         pass
-
 
 
 class ADDNORM(Layer):
@@ -173,21 +179,17 @@ class Encoder:
     def __init__(self, d_model=512, d_ff=2048, n_head=8, n_layers=6, dropout_rate=0.1):
         self.layers = [EncoderLayer(d_model, d_ff, n_head, dropout_rate) for _ in range(n_layers)]
 
-    def __call__(self, inputs_embedding, input_sequence, mask,return_attentions=False):
+    def __call__(self, inputs_embedding, input_sequence, mask, return_attentions=False):
         if return_attentions:
             attentions = []
         x = inputs_embedding
         for enc_layer in self.layers:
-            x, att = enc_layer(x,mask=mask)
+            x, att = enc_layer(x, mask=mask)
             if return_attentions:
                 attentions.append(att)
         return (x, attentions) if return_attentions else x
 
-np.random.random((3,4))
-np.random.random((3,1))
-enc = Encoder()
 
-enc(np.random.random((3,4)), np.random.random((3,1)),mask=None)
 
 
 def get_PAD_mask(q, k):  # todo check it latter
@@ -226,7 +228,6 @@ class Deocoder:
         return (x, decoder_attentions, encoder_attentions) if return_attentions else x
 
 
-
 class Transformer:
     def __init__(self, maxlen, d_model=512, d_ff=2048, n_head=8, d_v=64, layers=2, dropout=0.1):
         self.input_embedding = None  # dot it here
@@ -237,17 +238,18 @@ class Transformer:
         self.linear = None
         self.softmax = None
 
+
 class Transformer(keras.Model):
-    def __init__(self,in_dim=784, out_dim=10,d_model=512,n_layers=10,dp_rate=0.5):
+    def __init__(self, in_dim=784, out_dim=10, d_model=512, n_layers=10, dp_rate=0.5):
         self.n_model = d_model
         self.n_layers = n_layers
         self.dp_rate = dp_rate
         self.out_dim = out_dim
 
-        super(Transformer,self).__init__(name='Transformer')
+        super(Transformer, self).__init__(name='Transformer')
 
         self.input_layer = keras.layers.Input(shape=(784,))
         for i in range(self.n_layers):
-            exec(f"self.dense_{i+1} = keras.layers.Dense(d_model,activation='relu',name='dense_{i+1}')")
+            exec(f"self.dense_{i + 1} = keras.layers.Dense(d_model,activation='relu',name='dense_{i + 1}')")
             if self.dp_rate > 0:
                 exec(f"self.dp_{i + 1} = keras.layers.Dropout(dp_rate,name='dp_{i + 1}')")
