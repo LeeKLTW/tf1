8d8bd8998a5d351ddc89fea3b2588e52a7ef3e7e
LeeKLTW <LeeKLTW@gmail.com>
2019-05-29 11:46:51 +0800

    the simple viable attention

--- simple_attention.py
+++ simple_attention.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras import backend as K
 
 
 class Attention(keras.layers.Layer):
@@ -10,10 +11,13 @@ class Attention(keras.layers.Layer):
         super().__init__(**kwargs)
 
     def build(self, input_shape):
-        shape = tf.TensorShape([self.output_dim])
-        self.WQ = self.add_weight(name='WQ', shape=shape.as_list(), initializer='glorot_normal', trainable=True)
-        self.WK = self.add_weight(name='WQ', shape=shape.as_list(), initializer='glorot_normal', trainable=True)
-        self.WV = self.add_weight(name='WQ', shape=shape.as_list(), initializer='glorot_normal', trainable=True)
+        shape = tf.TensorShape([self.output_dim, self.output_dim])
+        self.WQ = self.add_weight(name='WQ', shape=shape.as_list(), initializer='glorot_normal', trainable=True,
+                                  dtype='float32')
+        self.WK = self.add_weight(name='WQ', shape=shape.as_list(), initializer='glorot_normal', trainable=True,
+                                  dtype='float32')
+        self.WV = self.add_weight(name='WQ', shape=shape.as_list(), initializer='glorot_normal', trainable=True,
+                                  dtype='float32')
         # use as_list() to fix TypeError: unsupported operand type(s) for /=: 'float' and 'Dimension'
         super().build(input_shape)
 
@@ -23,12 +27,11 @@ class Attention(keras.layers.Layer):
 
     def call(self, inputs):
         "inputs:[q,k,v]"
-        q = tf.matmul(inputs[0], self.WQ)
-        k = tf.matmul(inputs[1], self.WK)
-        v = tf.matmul(inputs[2], self.WV)
+        q = tf.matmul(inputs[0], self.WQ) #
+        k = tf.matmul(inputs[1], self.WK) #
+        v = tf.matmul(inputs[2], self.WV) #
 
-        # todo: fix dtype to float32, & check neccessary for transpose.
-        y = tf.matmul(q, k)
+        y = tf.matmul(q, k,transpose_b=True)
         y = keras.layers.Softmax()(y)
 
         y = tf.matmul(y, v)
@@ -41,9 +44,20 @@ class Attention(keras.layers.Layer):
     def from_config(cls, **config):
         return cls(**config)
 
-# import numpy as np
-# x_train = np.random.random((1,10,64))
+
+import numpy as np
+
+x_train = np.random.random((10, 64)).astype('float32')
+
+att = Attention(64)
+y= att([x_train, x_train, x_train])
+K.eval(y)
+y.shape
 #
-# att = Attention(64)
-# att([x_train,x_train,x_train])
-# att.output_dim
+#
+#
+# x = keras.layers.Input(shape=(10,64,))
+# y = Attention(64)(x)
+#
+# model = keras.Model(inputs=[x],outputs=[y])
+# model.compile()
